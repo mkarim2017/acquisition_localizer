bbfd379af2a92c621662015a629d587be9f11c07
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-11-10 01:44:57 +0000

    update

--- acquisition_localizer.sh
+++ acquisition_localizer.sh
@@ -4,24 +4,21 @@ BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 
 # source PGE env
 export PYTHONPATH=$BASE_PATH:$PYTHONPATH
-export PATH=$BASE_PATH:$PATH
-export PYTHONPATH=${PYTHONPATH}:${HOME}/verdi/etc
-export PYTHONPATH=${HOME}/verdi/etc:$BASE_PATH:$PYTHONPATH
+export PYTHONPATH=${PYTHONPATH}:${HOME}/verdi/etc 
 export PATH=$BASE_PATH:$PATH
 
 # source environment
 source $HOME/verdi/bin/activate
 
-
 echo "##########################################" 1>&2
-echo -n "Running sling_acquisitions: " 1>&2
+echo -n "Running sling_acquisitions.py on $1: " 1>&2
 date 1>&2
-/usr/bin/python $BASE_PATH/sling_acquisitions.py > sling_acquisitions.log 2>&1
+$BASE_PATH/sling_acquisitions.py $BASE_PATH/$1 > sling_acquisitions.log 2>&1
 STATUS=$?
-echo -n "Finished running sling_acquisitions: " 1>&2
+echo -n "Finished running $1 sling_acquisitions.py: " 1>&2
 date 1>&2
 if [ $STATUS -ne 0 ]; then
-  echo "Failed to run sling_acquisitions." 1>&2
+  echo "Failed to run $1 sling_acquisitions.py." 1>&2
   cat sling_acquisitions.log 1>&2
   echo "{}"
   exit $STATUS
