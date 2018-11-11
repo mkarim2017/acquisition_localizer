11db3c748d73688ea04f10511c1369382b245e85
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-11-11 15:21:41 +0000

    update

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # clone spyddder-man to be moved to its final location by docker builder
-git clone -b standard-product https://github.com/mkarim2017/spyddder-man.git
+git clone -b bc-cluster https://github.com/mkarim2017/spyddder-man.git
 
-# clone qquery to be moved to its final location by docker builder
-git clone -b standard-product https://github.com/mkarim2017/qquery.git
 
