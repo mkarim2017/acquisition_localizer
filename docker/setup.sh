86cb996101c0ca1b37b556e72a2bff30a92da275
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-11-14 14:33:30 +0000

    update

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # clone spyddder-man to be moved to its final location by docker builder
-git clone -b bc-cluster https://github.com/spyddder-man.git
+git clone -b master https://github.com/hysds/spyddder-man.git
 
 
